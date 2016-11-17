c4d3ec762481fbed7c45e2baa9a2c03377274a88
Kristen Simmonds <kristenmsimmonds@gmail.com>
2016-11-17 00:38:45 +0000

    adding signup

--- app/actions.rb
+++ app/actions.rb
@@ -4,3 +4,7 @@ get '/' do
     erb(:index)
 end
     
+get '/signup' do             #if the user navigates to the path '/signup'
+    @user = User.new         #setup empty @user object
+    erb(:signup)             #render "app/views/signup.erb"
+end
