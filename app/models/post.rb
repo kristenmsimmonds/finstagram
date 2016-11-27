294792aa2e24bc28c548142362e91d2e05dcb023
Kristen Simmonds <kristenmsimmonds@gmail.com>
2016-11-27 15:59:52 +0000

    class work week 5

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,13 +3,15 @@ class Post < ActiveRecord::Base
     belongs_to :user
     has_many :comments
     has_many :likes
+
+    validates_presence_of :photo_url, :user
     
     def humanized_time_ago
         time_ago_in_seconds = Time.now - self.created_at
         time_ago_in_minutes = time_ago_in_seconds / 60
         
         if time_ago_in_minutes >= 60
-            "#{(time_ago_in_minutes / 60).to_i} minutes ago"
+            "#{(time_ago_in_minutes / 60).to_i} hours ago"
         else
             "#{time_ago_in_minutes} minutes ago"
         end
