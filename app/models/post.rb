c360fbe2864a567e11e37d44ba3e9dc2affdf11a
Kristen Simmonds <kristenmsimmonds@gmail.com>
2016-11-28 18:20:43 +0000

    end of week 5 plus tweaking

--- app/models/post.rb
+++ app/models/post.rb
@@ -9,11 +9,23 @@ class Post < ActiveRecord::Base
     def humanized_time_ago
         time_ago_in_seconds = Time.now - self.created_at
         time_ago_in_minutes = time_ago_in_seconds / 60
+        time_ago_in_hours = time_ago_in_minutes / 60
+        time_ago_in_days = time_ago_in_hours / 24
         
-        if time_ago_in_minutes >= 60
-            "#{(time_ago_in_minutes / 60).to_i} hours ago"
+#        if time_ago_in_minutes >= 60
+#            "#{(time_ago_in_minutes / 60).to_i} hours ago"
+#        else
+#            "#{time_ago_in_minutes} minutes ago"
+#        end
+        
+        if time_ago_in_hours >= 24
+            "#{(time_ago_in_days).round} days ago"
+        elsif time_ago_in_minutes >= 60
+            "#{(time_ago_in_hours).round} hours ago"
+        elsif time_ago_in_seconds >= 60
+            "#{(time_ago_in_minutes).round} minutes ago"
         else
-            "#{time_ago_in_minutes} minutes ago"
+            "#{(time_ago_in_seconds).round} seconds ago"
         end
     end
     
